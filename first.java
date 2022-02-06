b4310cce0a077581a379e71a027c070016ea430a
Dineshkumar <dineshaix7612@gmail.com>
2022-02-06 18:51:54 +0530

    with eclipse

--- first.java
+++ first.java
@@ -1,9 +1,9 @@
-public first
+public class first
 {
 public static void main(String [] args)
 {
 System.out.println("This is from Github");
-System.out.println("This is from local laptop");'
+System.out.println("This is from local laptop");
 System.out.println("This is from agin git");
 System.out.println("This is from local laptop again");
 }
